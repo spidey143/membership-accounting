eed156e10327be5905816adba1d63dc29c7d10df
Tim-Shelll <tiim.shelll@gmail.com>
2022-02-09 10:44:30 +0300

    First upload
    Реализованы переходы между страницами без авторизации

--- main.py
+++ main.py
@@ -1,42 +1,34 @@
-'''
-Widget animation
-================
-
-This example demonstrates creating and applying a multi-part animation to
-a button widget. You should see a button labelled 'plop' that will move with
-an animation when clicked.
-'''
-
-import kivy
-kivy.require('1.0.7')
-
-from kivy.animation import Animation
 from kivy.app import App
-from kivy.uix.button import Button
+from kivy.properties import StringProperty
+from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
 
+from connected import Connected
 
-class TestApp(App):
+class Login(Screen):
+    def do_login(self, loginText, passwordText):
+        app = App.get_running_app()
 
-    def animate(self, instance):
-        # create an animation object. This object could be stored
-        # and reused each call or reused across different widgets.
-        # += is a sequential step, while &= is in parallel
-        animation = Animation(pos=(100, 100), t='out_bounce')
-        animation += Animation(pos=(200, 100), t='out_bounce')
-        animation &= Animation(size=(500, 500))
-        animation += Animation(size=(100, 50))
+        app.username = loginText
+        app.password = passwordText
 
-        # apply the animation on the button, passed in the "instance" argument
-        # Notice that default 'click' animation (changing the button
-        # color while the mouse is down) is unchanged.
-        animation.start(instance)
+        self.manager.transition = SlideTransition(direction="left")
+        self.manager.current = 'connected'
+
+    def resetForm(self):
+        self.ids['login'].text = ""
+        self.ids['password'].text = ""
+
+class LoginApp(App):
+    username = StringProperty(None)
+    password = StringProperty(None)
 
     def build(self):
-        # create a button, and  attach animate() method as a on_press handler
-        button = Button(size_hint=(None, None), text='plop',
-                        on_press=self.animate)
-        return button
 
+        sm = ScreenManager()
+        sm.add_widget(Login(name="login"))
+        sm.add_widget(Connected(name="connected"))
+
+        return sm
 
 if __name__ == '__main__':
-    TestApp().run()
+    LoginApp().run()
