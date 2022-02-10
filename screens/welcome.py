cc9ff95cc537231d2319f15c491993df08fd7eae
yuri.silenok <yuri.silenok@gmail.com>
2022-02-11 00:40:38 +0300

    Разделил дизайн и логику по разным папкам

--- screens/welcome.py
+++ screens/welcome.py
@@ -1,10 +1,22 @@
-from kivy.uix.screenmanager import Screen
-from kivy.uix.screenmanager import SlideTransition
-
+from kivy.uix.screenmanager import Screen, SlideTransition
+from kivyauth.utils import auto_login, login_providers
+from kivyauth.google_auth import initialize_google
 
 
 class Welcome(Screen):
 
+    def on_start(self):
+        if auto_login(login_providers.google):
+            self.current_provider = login_providers.google
+
     def login(self):
+        print('login')
+        initialize_google(self.after_login, self.error_listener)
+
+    def after_login(self, **qwargs):
+        print(qwargs)
         self.manager.transition = SlideTransition(direction="left")
         self.manager.current = 'clients'
+
+    def error_listener(self, **qwargs):
+        print(qwargs)
